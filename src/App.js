e9d6e289f908eea9ab4bfb25975f2320cd3e15e4
wassim <b.wassim1993@hotmail.fr>
2021-10-21 12:04:09 +0100

    toggle feature implemented

--- src/App.js
+++ src/App.js
@@ -1,51 +1,64 @@
 import React, {Component} from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
-class App extends Component{
+
+class App extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            tasks : [
+            tasks: [
                 {
-                    id:1,
-                    text:'doctor appointment',
-                    day:'5 feb 2021',
-                    reminder:true,
+                    id: 1,
+                    text: 'doctor appointment',
+                    day: '5 feb 2021',
+                    reminder: true,
                 },
                 {
-                    id:2,
-                    text:'school meeting',
-                    day:'20 dec 2021',
-                    reminder:false,
+                    id: 2,
+                    text: 'school meeting',
+                    day: '20 dec 2021',
+                    reminder: false,
                 },
                 {
-                    id:3,
-                    text:'Shopping',
-                    day:'10 april 2021',
-                    reminder:true,
+                    id: 3,
+                    text: 'Shopping',
+                    day: '10 april 2021',
+                    reminder: true,
                 }
             ]
 
         }
     }
+
     deleteTask = (id) => {
         //const tasks = [...this.state.tasks].filter((task) => task.id !== id)
-        this.setState({tasks:this.state.tasks.filter((task) => task.id !== id)},()=>{
+        this.setState({tasks: this.state.tasks.filter((task) => task.id !== id)}, () => {
             console.log('state changed')
         })
     }
-  render() {
-      return (
-          <div className="container">
-              <Header/>
-              <Tasks tasks={this.state.tasks} onDelete={this.deleteTask}/>
-          </div>
-      );
-  }
+
+    toggleReminder = (id) => {
+        const foundTask = this.state.tasks.map((task) => task.id === id ? {
+            ...task,
+            reminder: !task.reminder
+        }: task)
+        this.setState({tasks:foundTask})
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <Header/>
+                {
+                    this.state.tasks.length > 0 ?
+                        <Tasks tasks={this.state.tasks} onDelete={this.deleteTask}
+                               onDoubleClick={this.toggleReminder}/> : 'No Available tasks'
+                }
+            </div>
+        );
+    }
 }
-
-
 
 
 export default App;
